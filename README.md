b426271fa3462de13c5be396bb41cf60613701a2
huhuang03 <huhuang03@gmail.com>
2015-09-13 23:04:43 +0800

    inital

---
+++ README.md
@@ -0,0 +1,6 @@
+IOS测试程序
+1. UIWindowTest
+手动加载window，而不是通过设置main stroyboard自动加载
+
+2. AnimiationTest
+尝试各种动画
---
+++ UIWindowTest/UIWindowTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UIWindowTest
+//
+//  Created by Tonghu_Yi on 9/13/15.
+//  Copyright (c) 2015 Tonghu_Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("coming here")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
